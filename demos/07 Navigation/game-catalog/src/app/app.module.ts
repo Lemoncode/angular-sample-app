91d6038d342a17d5b66a93ef0ad982fce08048e6
jaimesalas <jaime.salas@lemoncode.net>
2019-11-09 12:47:05 +0100

    navigation demo started

---
+++ demos/07 Navigation/game-catalog/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { GameSummaryComponent } from './game/game-summary.component';
+import { GameStockService } from './services/game-stock.service';
+import { GameSellersComponent } from './game/game-sellers.component';
+import { CreateGameComponent } from './game/create-game.component';
+import { GameListComponent } from './game/game-list.component';
+
+import { appRoutes } from './app.routes';
+import { CreateSellerComponent } from './seller/create-seller.component';
+import { SellerCategoryService } from './services/seller-category.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameSummaryComponent,
+    GameSellersComponent,
+    CreateGameComponent,
+    GameListComponent,
+    CreateSellerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    GameStockService,
+    SellerCategoryService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
