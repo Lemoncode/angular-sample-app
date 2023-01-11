3fc3ff6e40434032944e52800d09674505c6b78c
Braulio Diez Botella <braulio.diez@gmail.com>
2023-01-11 17:48:01 +0100

    02 starting point

--- demos/02 Displaying Collections/game-catalog/src/app/app.component.ts
+++ demos/02 Displaying Collections/game-catalog/src/app/app.component.ts
@@ -1,34 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Game } from './models/game.model';
+import { Component } from '@angular/core';
+import { Game } from './model/game.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'Displaying Collection Demo';
-  games: Game[];
+export class AppComponent {
+  title = 'game-catalog';
+  private games: Game[];
+  game: Game = new Game("");
 
-  constructor() {}
+  constructor() {
+    console.log('** Constructor called **');
+    this.games = [
+      new Game('Super Mario Bros', '13 September 1985'),
+      new Game('Legend of Zelda', '21 February 1986'),
+      new Game('Sonic', '26 June 1981'),
+    ];
+  }
 
   ngOnInit(): void {
-    this.games = [
-      new Game(
-        'Super Mario Bros',
-        '13 September 1985',
-        'http://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros_image1280w.jpg',
-      ),
-      new Game(
-        'Legend of Zelda',
-        '21 February 1986',
-        'http://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/main_element/public/media/image/2013/06/227201-analisis-legend-zelda-oracle-ages/seasons.jpg?itok=A8pOGd_f',
-      ),
-      new Game(
-        'Sonic',
-        '26 June 1981',
-        'https://i.ytimg.com/vi/dfFd7Bu6xnc/hqdefault.jpg',
-      ),
-    ];
+    console.log('** ngOnInit called **');
+    this.game = this.games[0];
   }
 }
--- demos/02 Displaying Collections/game-catalog/src/app/app.module.ts
+++ demos/02 Displaying Collections/game-catalog/src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GameSummaryComponent } from './game/game-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GameSummaryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
